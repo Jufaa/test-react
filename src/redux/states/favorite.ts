42819fb3c4e0fc5ea17be5153cd5f93abffc9a5f
Juan Cruz Francitorra <jufovich@gmail.com>
2023-05-20 14:14:58 -0300

    changes

--- src/redux/states/favorite.ts
+++ src/redux/states/favorite.ts
@@ -1,29 +1,32 @@
 import { Person, Types } from '@/models';
 import { getLocalStorage, setLocalStorage } from '@/util';
-import {createSlice, current} from '@reduxjs/toolkit'
+import { createSlice, current, PayloadAction } from '@reduxjs/toolkit';
 
-
-const initialState: Person[] = []
-
+const initialState: Person[] = [];
 
 export const favoriteSlice = createSlice({
-    name: 'favorite',
-    initialState: getLocalStorage(Types.FAVORITE)
-     ? JSON.parse(getLocalStorage(Types.FAVORITE) as string) 
-     : initialState,
-    reducers: {
-        addFavorite: (action) => {
-            setLocalStorage(Types.FAVORITE, action.payload);
-            return action.payload
-        },
-        removeFavorite: (state, action) => {
-            const filteredState= current(state).filter((p:Person) => p.id !== action.payload.id)
-            setLocalStorage(Types.FAVORITE, filteredState);
-            return filteredState
+  name: 'favorite',
+  initialState: getLocalStorage(Types.FAVORITE)
+    ? JSON.parse(getLocalStorage(Types.FAVORITE) as string)
+    : initialState,
+  reducers: {
+    addFavorite: (state, action: PayloadAction<Person[]>) => {
+      const filteredState = current(state).filter(
+        (p: Person) => p.id !== action.payload[0].id
+      );
+      const newState = [...filteredState, ...action.payload];
+      setLocalStorage(Types.FAVORITE, newState);
+      return newState;
+    },
+    removeFavorite: (state, action: PayloadAction<Person>) => {
+      const filteredState = current(state).filter(
+        (p: Person) => p.id !== action.payload.id
+      );
+      setLocalStorage(Types.FAVORITE, filteredState);
+      return filteredState;
+    },
+  },
+});
 
-        }
-    }
-})
-
-export const {addFavorite, removeFavorite} = favoriteSlice.actions;
+export const { addFavorite, removeFavorite } = favoriteSlice.actions;
 export default favoriteSlice.reducer;
